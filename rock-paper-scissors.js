1875e1acca3e4045c873f8fffd2f67594141847b
Similo <171865838+Chief-99@users.noreply.github.com>
2024-12-17 12:47:51 +0200

    Write out the getComputerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,5 +5,15 @@
 // If computerChoice beats userChoice, display defeat message
 
 function getComputerChoice() {
-  
+  let number = Math.random();
+  let computerChoice = '';
+  if (number >= 0 && number < 1 / 3) {
+    computerChoice = 'rock';
+  } else if (number >= 1 / 3 && number < 2 / 3) {
+    computerChoice = 'paper';
+  } else {
+    computerChoice = 'scissors';
+  }
+
+  return computerChoice;
 }
