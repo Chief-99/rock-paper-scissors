a3dee0c1c22caf17e993bf31d8e680902873250d
Similo <171865838+Chief-99@users.noreply.github.com>
2024-12-17 12:40:13 +0200

    Create getComputerChoice function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,9 @@
+// Create a variable for the computer choice
+// Randomly assign either 'rock', 'paper' or 'scissors' to computerChoice
+// Get user's choice from a prompt
+// If userChoice beats computerChoice, display victory message
+// If computerChoice beats userChoice, display defeat message
+
+function getComputerChoice() {
+  
+}
