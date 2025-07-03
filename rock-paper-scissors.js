5a061fcc0b3c8ca4c6076ae82de51c7574374042
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-03 13:43:18 +0200

    Increment scores by one each time there is a winner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -24,22 +24,28 @@ function playRound(humanChoice, computerChoice) {
         if (computerChoice.toLowerCase() === 'rock') {
             return 'Tie game!';
         } else if(computerChoice.toLowerCase() === 'paper') {
+            computerScore += 1;
             return 'You lose! Paper beats rock.'
         } else if (computerChoice.toLowerCase() === 'scissors') {
+            humanScore += 1;
             return 'You win! Rock beats scissors.';
         }
     } else if (humanChoice.toLowerCase() === 'paper') {
         if (computerChoice.toLowerCase() === 'rock') {
+            humanScore += 1;
             return 'You win! Paper beats rock.';
         } else if(computerChoice.toLowerCase() === 'paper') {
             return 'Tie game!'
         } else if (computerChoice.toLowerCase() === 'scissors') {
+            computerScore += 1;
             return 'You lose! Scissors beats paper.';
         }
     } else if (humanChoice.toLowerCase() === 'scissors'){
         if (computerChoice.toLowerCase() === 'rock') {
+            computerScore += 1;
             return 'You lose! Rock beats scissors.';
         } else if(computerChoice.toLowerCase() === 'paper') {
+            humanScore += 1;
             return 'You win! Scissors beats paper.';
         } else if (computerChoice.toLowerCase() === 'scissors') {
             return 'Tie game!';
