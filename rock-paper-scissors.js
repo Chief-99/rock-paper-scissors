bb69df5576994d9f269a9263b6fc5e81e3153a4e
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-03 14:38:45 +0200

    Declare `playRound()` inside `playGame()`

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -17,51 +17,51 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    let human = humanChoice.toLowerCase();
-    let computer = computerChoice.toLowerCase();
 
-    if (human === 'rock') {
-        if (computer === 'rock') {
-            return 'Tie game!';
-        } else if(computer === 'paper') {
-            computerScore += 1;
-            return 'You lose! Paper beats rock.'
-        } else if (computer === 'scissors') {
-            humanScore += 1;
-            return 'You win! Rock beats scissors.';
-        }
-    } else if (human === 'paper') {
-        if (computer === 'rock') {
-            humanScore += 1;
-            return 'You win! Paper beats rock.';
-        } else if(computer === 'paper') {
-            return 'Tie game!'
-        } else if (computer === 'scissors') {
-            computerScore += 1;
-            return 'You lose! Scissors beats paper.';
-        }
-    } else if (human === 'scissors'){
-        if (computer === 'rock') {
-            computerScore += 1;
-            return 'You lose! Rock beats scissors.';
-        } else if(computer === 'paper') {
-            humanScore += 1;
-            return 'You win! Scissors beats paper.';
-        } else if (computer === 'scissors') {
-            return 'Tie game!';
-        }
-    }
-}
-
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
- 
+    
     while (humanScore < 5 || computerScore < 5) {
         playRound();
+        
+    }
+
+    function playRound() {
+        let human = String(getHumanChoice()).toLowerCase();
+        let computer = String(getComputerChoice()).toLowerCase();
+    
+        if (human === 'rock') {
+            if (computer === 'rock') {
+                return 'Tie game!';
+            } else if(computer === 'paper') {
+                computerScore += 1;
+                return 'You lose! Paper beats rock.'
+            } else if (computer === 'scissors') {
+                humanScore += 1;
+                return 'You win! Rock beats scissors.';
+            }
+        } else if (human === 'paper') {
+            if (computer === 'rock') {
+                humanScore += 1;
+                return 'You win! Paper beats rock.';
+            } else if(computer === 'paper') {
+                return 'Tie game!'
+            } else if (computer === 'scissors') {
+                computerScore += 1;
+                return 'You lose! Scissors beats paper.';
+            }
+        } else if (human === 'scissors'){
+            if (computer === 'rock') {
+                computerScore += 1;
+                return 'You lose! Rock beats scissors.';
+            } else if(computer === 'paper') {
+                humanScore += 1;
+                return 'You win! Scissors beats paper.';
+            } else if (computer === 'scissors') {
+                return 'Tie game!';
+            }
+        }
     }
 }
