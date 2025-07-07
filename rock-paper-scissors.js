272c110a6962da07618f8e63549860b597ac6a2f
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-07 13:40:15 +0200

    Change while loop condition and pla`playRound()` result display
    I had put an and operator instead of or in the while
    loop condition. I also changed the `playRound()` function
    to log the results of the round in the console instead
    of returning them.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,67 +1,71 @@
 
-function getComputerChoice() {
-    let choiceNumber = Math.random();
 
-    if (choiceNumber <= 0.3333) {
-        return 'rock';
-    } else if (choiceNumber > 0.3333 & choiceNumber <= 0.6666) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }
-}
-
-function getHumanChoice() {
-    let choice = prompt('Please enter your choice', '');
-    
-    return choice;
-}
 
 
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
-    
-    while (humanScore < 5 || computerScore < 5) {
+
+    while (humanScore < 5 && computerScore < 5) {
         playRound();
-        
-    }
+
+    } 
 
     function playRound() {
         let human = String(getHumanChoice()).toLowerCase();
         let computer = String(getComputerChoice()).toLowerCase();
-    
+
         if (human === 'rock') {
             if (computer === 'rock') {
-                return 'Tie game!';
-            } else if(computer === 'paper') {
+                console.log('Tie game!');
+            } else if (computer === 'paper') {
                 computerScore += 1;
-                return 'You lose! Paper beats rock.'
+                console.log('You lose! Paper beats rock.');
             } else if (computer === 'scissors') {
                 humanScore += 1;
-                return 'You win! Rock beats scissors.';
+                console.log('You win! Rock beats scissors.');
             }
         } else if (human === 'paper') {
             if (computer === 'rock') {
                 humanScore += 1;
-                return 'You win! Paper beats rock.';
-            } else if(computer === 'paper') {
-                return 'Tie game!'
+                console.log('You win! Paper beats rock.');
+            } else if (computer === 'paper') {
+                console.log('Tie game!');
             } else if (computer === 'scissors') {
                 computerScore += 1;
-                return 'You lose! Scissors beats paper.';
+                console.log('You lose! Scissors beats paper.');
             }
-        } else if (human === 'scissors'){
+        } else if (human === 'scissors') {
             if (computer === 'rock') {
                 computerScore += 1;
-                return 'You lose! Rock beats scissors.';
-            } else if(computer === 'paper') {
+                console.log('You lose! Rock beats scissors.');
+            } else if (computer === 'paper') {
                 humanScore += 1;
-                return 'You win! Scissors beats paper.';
+                console.log('You win! Scissors beats paper.');
             } else if (computer === 'scissors') {
-                return 'Tie game!';
+                console.log('Tie game!');
             }
         }
     }
+
+    function getComputerChoice() {
+        let choiceNumber = Math.random();
+
+        if (choiceNumber <= 0.3333) {
+            return 'rock';
+        } else if (choiceNumber > 0.3333 & choiceNumber <= 0.6666) {
+            return 'paper';
+        } else {
+            return 'scissors';
+        }
+    }
+
+    function getHumanChoice() {
+        let choice = prompt('Please enter your choice', '');
+
+        return choice;
+    }
 }
+
+// playGame();
