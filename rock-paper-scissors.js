726130200c0a2ce0fa06cb8f8505047e86a7cff4
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-11 13:37:26 +0200

    Display score in div for all button events

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -19,73 +19,33 @@ function playRound(event) {
 
     if (human === 'rock') {
         if (computer === 'rock') {
-            display.textContent = `Tie game! \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`
+            display.textContent = `Tie game! \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`;
         } else if (computer === 'paper') {
             computerScore += 1;
-            console.log(
-                `You lose! Paper beats rock.
-                    
-                    Player: ${humanScore}
-                    Computer: ${computerScore}`
-            );
+            display.textContent = `You lose! Paper beats rock. \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`;
         } else if (computer === 'scissors') {
             humanScore += 1;
-            console.log(
-                `You win! Rock beats scissors.
-                    
-                    Player: ${humanScore}
-                    Computer: ${computerScore}`
-            );
+            display.textContent = `You win! Rock beats scissors. \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`;
         }
     } else if (human === 'paper') {
         if (computer === 'rock') {
             humanScore += 1;
-            console.log(
-                `You win! Paper beats rock.
-                    
-                    Player: ${humanScore}
-                    Computer: ${computerScore}`
-            );
+            display.textContent = `You win paper beats rock. \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`;
         } else if (computer === 'paper') {
-            console.log(
-                `Tie game!
-                    
-                    Player: ${humanScore}
-                    Computer: ${computerScore}`
-            );
+            display.textContent = `Tie game! \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`;
         } else if (computer === 'scissors') {
             computerScore += 1;
-            console.log(
-                `You lose! Scissors beats paper.
-                    
-                    Player: ${humanScore}
-                    Computer: ${computerScore}`
-            );
+            display.textContent = `You lose! Scissors beats paper. \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`;
         }
     } else if (human === 'scissors') {
         if (computer === 'rock') {
             computerScore += 1;
-            console.log(
-                `You lose! Rock beats scissors.
-                    
-                    Player: ${humanScore}
-                    Computer: ${computerScore}`
-            );
+            display.textContent = `You lose! Rock beats scissors. \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`;
         } else if (computer === 'paper') {
             humanScore += 1;
-            console.log(
-                `You win! Scissors beats paper.
-                    
-                    Player: ${humanScore}
-                    Computer: ${computerScore}`
-            );
+            display.textContent = `You win! Scissors beat paper. \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`;
         } else if (computer === 'scissors') {
-            console.log(
-                `Tie game!
-                    
-                    Player: ${humanScore}
-                    Computer: ${computerScore}`
-            );
+            display.textContent = `Tie game! \n\nPlayer: ${humanScore} \nComputer: ${computerScore}`;
         }
     }
 
